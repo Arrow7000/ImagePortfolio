1dab04b78b33b6add70b6cde59f5e12b63cfa188
Aron Adler <aron@adler.dev>
2020-01-24 02:26:41 +1100

    Start of image conversion

--- SyncImages/ImageConversion.fs
+++ SyncImages/ImageConversion.fs
@@ -1,8 +1,33 @@
 ﻿module ImageConversion
 
-open Veldrid.ImageSharp
 
 open LocalImages
 
 
-//let generateSizedImgs path = Veldrid.
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.Processing
+
+open System.IO
+
+let resizeImg maxSideSize (path : string) =
+    use image = Image.Load(path)
+
+    let (newWidth,newHeight) =
+        let w = image.Width
+        let h = image.Height
+        if w > h then
+            maxSideSize, h / w * maxSideSize
+        else w / h * maxSideSize, maxSideSize
+
+    use copy = image.Clone()
+
+    let operation =
+        fun (x : IImageProcessingContext<PixelFormats.Rgba32>) -> x.Resize(newWidth, newHeight) |> ignore
+
+    copy.Mutate operation
+
+    let stream = new MemoryStream()
+    let encoder = new Formats.Jpeg.JpegEncoder()
+    copy.Save(stream, encoder)
+
+    stream
